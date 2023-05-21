8523360d1caa4fe41ea13eb2fae52b43e2bb96de
Brandon <brandon.lagasse@gmail.com>
2023-05-21 16:13:43 -0700

    Initial commit

---
+++ src/main/java/lagasse/c195/HelloController.java
@@ -0,0 +1,14 @@
+package lagasse.c195;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Button buttonLogin;
+    @FXML
+    private void login(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lagasse.c195 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lagasse.c195 to javafx.fxml;
+    exports lagasse.c195;
+}
