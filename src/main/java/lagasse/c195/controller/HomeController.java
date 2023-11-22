500aa13182f091c0548dca878e25785c3e77c798
blagass <brandon.lagasse@gmail.com>
2023-11-22 02:41:43 -0700

    ya

--- src/main/java/lagasse/c195/controller/HomeController.java
+++ src/main/java/lagasse/c195/controller/HomeController.java
@@ -1,5 +1,7 @@
 package lagasse.c195.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import lagasse.c195.model.Customer;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -51,6 +54,8 @@ public class HomeController implements Initializable {
     @FXML
     private TableView customerTableView;
 
+    private ObservableList<Customer> allCustomers = FXCollections.observableArrayList();
+
     public void onAddCustomer(ActionEvent actionEvent) {
 
     }
@@ -72,8 +77,16 @@ public class HomeController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         customerNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
         customerAddressCol.setCellValueFactory(new PropertyValueFactory<>("address"));
+        customerCountryCol.setCellValueFactory(new PropertyValueFactory<>("country"));
+        customerPhoneCol.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        customerPostalCol.setCellValueFactory(new PropertyValueFactory<>("postal"));
+        customerStateCol.setCellValueFactory(new PropertyValueFactory<>("state"));
+
+        customerTableView.setItems(allCustomers);
+
 
     }
 }
